20cc61d1abe9aef12e9253fef6ee94820b6d1427
Irina Kamkina <kamkina2312@gmail.com>
2023-05-26 21:42:38 +0300

    2 sliders

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,62 @@
 import '../scss/style.scss'
+import Swiper, { Navigation, Pagination } from 'swiper'
 
-console.log('Works!')
+/* Swiper
+ **************************************************************/
+var swiper = Swiper
+var init = false
+
+/* Which media query
+ **************************************************************/
+function swiperMode() {
+  let mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)')
+
+  // Enable (for mobile)
+  if (mobile.matches) {
+    if (!init) {
+      init = true
+      swiper = new Swiper('.swiper', {
+        modules: [Navigation, Pagination],
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1.3,
+        spaceBetween: 16,
+
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          paginationClicable: true
+        }
+      })
+    }
+  }
+
+  // Disable
+  else {
+    swiper.distroy
+    init = false
+  }
+}
+
+/* On Load
+ **************************************************************/
+window.addEventListener('load', function () {
+  swiperMode()
+})
+
+/* On Resize
+ **************************************************************/
+window.addEventListener('resize', function () {
+  swiperMode()
+})
+
+const burgerBtn = document.querySelector('.header__burger-btn')
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('open')
+  document.querySelector('.header__logo').classList.toggle('open')
+  document.querySelector('.burger__container').classList.toggle('open')
+  document.querySelector('.header__contacts').classList.toggle('open')
+  document.querySelector('.header__servise').classList.toggle('open')
+})
